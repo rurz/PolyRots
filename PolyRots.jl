331f48e4975801246f12d23a9bcce565764d9ae9
arurz <arurz@inaoep.mx>
2021-05-21 20:10:37 -0500

    Folder structure added
    * data/img_origin for the source images
    * data/img_target for the transformed images
    * data/data_target for the transformed arrays

--- PolyRots.jl
+++ PolyRots.jl
@@ -22,36 +22,37 @@ mb = zeros((2 * j + 1, 2 * j + 1))
 
 @info "Performing transformation in R"
 for x in -j:j
-   for y in -j:j
-       mr[x + j + 1, y + j + 1] = real(Fqθ(ang, x, y, j, img_r))
-   end
+    for y in -j:j
+        mr[x + j + 1, y + j + 1] = real(Fqθ(ang, x, y, j, img_r))
+    end
 end
 
 @info "Performing transformation in G"
 for x in -j:j
-   for y in -j:j
-       mg[x + j + 1, y + j + 1] = real(Fqθ(ang, x, y, j, img_g))
-   end
+    for y in -j:j
+        mg[x + j + 1, y + j + 1] = real(Fqθ(ang, x, y, j, img_g))
+    end
 end
 
 @info "Performing transformation in B"
 for x in -j:j
-   for y in -j:j
-       mb[x + j + 1, y + j + 1] = real(Fqθ(ang, x, y, j, img_b))
-   end
+    for y in -j:j
+        mb[x + j + 1, y + j + 1] = real(Fqθ(ang, x, y, j, img_b))
+    end
 end
+
 
 simg = PixelsGT.imgrep(mr, mg, mb)
 
 @info "Writting the results"
-writedlm(joinpath(abspath(""), "data/imgr.dat"), mr)
-writedlm(joinpath(abspath(""), "data/imgg.dat"), mg)
-writedlm(joinpath(abspath(""), "data/imgb.dat"), mb)
+writedlm(joinpath(abspath(""), "data/data_target/array_r.dat"), mr)
+writedlm(joinpath(abspath(""), "data/data_target/array_g.dat"), mg)
+writedlm(joinpath(abspath(""), "data/data_target/array_b.dat"), mb)
 
 @info "Showing the transformed two-dimensional field"
 begin
     imshow(simg)
     box(false)
     axis("off")
-    savefig(joinpath(abspath(""), "data/imgrot.png"))
+    savefig(joinpath(abspath(""), "data/img_target/img_rot.png"))
 end
